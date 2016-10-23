6795ea531d814cdb7da8a2b2d926cbae46c48534
Stefan Motko <stefanmotko@gmail.com>
2016-10-23 14:06:16 +0200

    Set up route and view for prototype page

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var prototype = require('./routes/prototype');
 
 var app = express();
 
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/prototype', prototype);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/prototype.js
+++ routes/prototype.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET prototype page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Activity3D' });
+  res.render('prototype', { title: 'Activity3D' });
 });
 
 module.exports = router;
