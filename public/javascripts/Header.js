b29482d6f13d741f62f213f818c65b028b91237b
Stefan Motko <stefanmotko@gmail.com>
2016-10-24 17:32:36 +0200

    Added some descriptions for friendlier familiarization

--- public/javascripts/Header.js
+++ public/javascripts/Header.js
@@ -10,16 +10,17 @@
 function feature_test() {
     var a = new Layer(-100, -100, 0, 100, 75);
     var b = new Layer(100, 100, 200, 100, 75);
-    var c = new Layer(100, -100, 0, 100, 75);
+    var c = new Layer(0, 0, -200, 800, 600);
+    var d = new Layer(100, -150, 0, 100, 75);
     a.setOwnHTML("<p>Component A</p>");
     b.setOwnHTML("<p>Component B</p>");
-    c.setOwnHTML("<p>Component C</p>");
+    d.setOwnHTML("<p>Component D</p>");
     parentLayer.addChild(a);
     parentLayer.addChild(b);
     parentLayer.addChild(c);
+    c.addChild(d);
     a.addChild(new Connector(b, 1, 1, -1, -1));
-    a.addChild(new Connector(c, 1, 1, -1, -1));
+    a.addChild(new Connector(d, 1, 1, -1, -1));
     draw();
-    console.log(a.relativeCoordinates(lowestCommonAncestor(a, b)));
 }
 //# sourceMappingURL=Header.js.map
